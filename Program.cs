96fbede56b062bbfe4e6d3533170ed21e4037546
Kevin Le <kevinle108@gmail.com>
2021-03-30 15:57:01 -0400

    started Accounts

--- Program.cs
+++ Program.cs
@@ -6,65 +6,144 @@ namespace Day16
     {
         public static void Main(string[] args)
         {
-            Person[] persons =
-            {
-            new Teacher("Yung"),
-            new Student("Kevin"),
-            new Student("Tommy"),
-            new Student("Huy")
-        };
+            var a1 = new DepositAccount("Kevin", "individual", 500, 0.015);
+            var a2 = new LoanAccount("Dep", "individual", 30000, 0.07);
+            var a3 = new MortgageAccount("IVS LLC", "company", 80000, 0.04);
+            a1.DisplayAccountInfo();
+            a2.DisplayAccountInfo();
+            Console.WriteLine();
+            a1.Deposit(100);
+            a1.Withdraw(200);
 
-            for (int i = 0; i < persons.Length; i++)
+        }
+
+        public class Account
+        {
+            public struct Customer
             {
-                if (i == 0)
+                public string name;
+                public string type;
+            }
+
+            protected Customer _customer;
+            protected double _balance;
+            protected double _interestRate;
+
+            public Account(string customerName, string customerType, double balance, double interestRate)
+            {
+                _customer = new Customer { name = customerName, type = customerType };
+                _balance = balance;
+                _interestRate = interestRate;
+            }
+
+            public void Deposit(double amt) 
+            {
+                _balance += amt;
+                Console.WriteLine($"${amt} has been successfully deposited into {_customer.name}'s account.");
+                Console.WriteLine($" -> Updated Balance: ${_balance}");
+                Console.WriteLine();
+            }
+
+            public void DisplayAccountInfo()
+            {
+                Console.WriteLine($"Name: {_customer.name}, {_customer.type}");
+                Console.WriteLine($"Balance: ${_balance}");
+                Console.WriteLine($"Interest Rate: {_interestRate*100}%");
+                //Console.WriteLine(this.GetType());
+                if (this is DepositAccount) 
+                {                    
+                    Console.WriteLine("Deposit Account");
+                }
+                else if (this is LoanAccount)
                 {
-                    ((Teacher)persons[i]).Teach();
+                    Console.WriteLine("Loan Account");
+                }
+                else if (this is MortgageAccount)
+                {
+                    Console.WriteLine("Mortgage Account");
                 }
                 else
                 {
-                    ((Student)persons[i]).Study();
+                    Console.WriteLine("Invalid Account!");
                 }
+                Console.WriteLine();
             }
         }
 
-        public class Person
+        public class DepositAccount : Account
         {
-            protected string Name { get; set; }
-
-            public Person(string name)
+            public DepositAccount(string customerName, string customerType, double balance, double interestRate) : base(customerName, customerType, balance, interestRate)
             {
-                Name = name;
             }
 
-            public void SayHello()
+            public void Withdraw(double amt)
             {
-                Console.WriteLine("Hello! My name is " + Name);
+                _balance -= amt;
+                Console.WriteLine($"${amt} has been successfully withdrawn from {_customer.name}'s account.");
+                Console.WriteLine($" -> Updated Balance: ${_balance}");
+                Console.WriteLine();
             }
         }
 
-        public class Teacher : Person
+        public class LoanAccount : Account
         {
-            public Teacher(string name) : base(name)
+            public LoanAccount(string customerName, string customerType, double balance, double interestRate) : base(customerName, customerType, balance, interestRate)
             {
-            }
-
-            public void Teach()
-            {
-                Console.WriteLine(Name + " teaches");
             }
         }
 
-        public class Student : Person
+        public class MortgageAccount : Account
         {
-            public Student(string name) : base(name)
+            public MortgageAccount(string customerName, string customerType, double balance, double interestRate) : base(customerName, customerType, balance, interestRate)
             {
-            }
-
-            public void Study()
-            {
-                Console.WriteLine(Name + " studies");
             }
         }
+
+        
+
+
+
+
+
+
+        //public class Person
+        //{
+        //    protected string Name { get; set; }
+
+        //    public Person(string name)
+        //    {
+        //        Name = name;
+        //    }
+
+        //    public void SayHello()
+        //    {
+        //        Console.WriteLine("Hello! My name is " + Name);
+        //    }
+        //}
+
+        //public class Teacher : Person
+        //{
+        //    public Teacher(string name) : base(name)
+        //    {
+        //    }
+
+        //    public void Teach()
+        //    {
+        //        Console.WriteLine(Name + " teaches");
+        //    }
+        //}
+
+        //public class Student : Person
+        //{
+        //    public Student(string name) : base(name)
+        //    {
+        //    }
+
+        //    public void Study()
+        //    {
+        //        Console.WriteLine(Name + " studies");
+        //    }
+        //}
     }
 
 }
